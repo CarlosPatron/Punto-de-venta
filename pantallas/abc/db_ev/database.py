216d898b1e984819e49129c53680fd75cab299fb
CarlosPatron <witsamalababusa2@gmail.com>
2023-05-10 10:09:34 -0700

    Register product success

--- pantallas/abc/db_ev/database.py
+++ pantallas/abc/db_ev/database.py
@@ -7,12 +7,12 @@ class Database:
         self.password = '123456'
         self.database = 'db_puntoventa'
 
-    def connectDB(host, user, pwd, db):
+    def connectDB(self):
         cnx = mysql.connector.connect(
-            host = host,
-            user = user,
-            password = pwd,
-            database = db,
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database = self.database,
         )
 
         return cnx, cnx.cursor()
@@ -34,22 +34,22 @@ class Database:
         self.exeCmd(command)
 
     def exeCmd(self, command):
-        cursor, cnx = self.connectDB()
+        cnx, cursor = self.connectDB()
 
         cursor.execute(command)
         cnx.commit()
         cnx.close()
 
-    def selectCmd(opc, table, obj):
+    def selectCmd(self, opc, table, obj):
         if opc=='Producto':
-            command = f"INSERT INTO {table} (codigo, nombre, precio_compra, precio_venta, stock) VALUES ('{obj.codigo}', '{obj.nombre}', {float(obj.precio_compra)}, {float(obj.precio_venta)}, {int(obj.stock)};)"
+            command = f"INSERT INTO {table} (codigo, nombre, precio_compra, precio_venta, stock) VALUES ('{obj.codigo}', '{obj.nombre}', {float(obj.precio_compra)}, {float(obj.precio_venta)}, {int(obj.stock)});"
         elif opc=='Empleado':
-            command = f"INSERT INTO {table} (nombre, ap_p, ap_m, phone, rol) VALUES ('{obj.nombre}', '{obj.ap_p}', '{obj.ap_m}', '{obj.phone}', '{obj.rol}';)"
+            command = f"INSERT INTO {table} (nombre, ap_p, ap_m, phone, rol) VALUES ('{obj.nombre}', '{obj.ap_p}', '{obj.ap_m}', '{obj.phone}', '{obj.rol}');"
         elif opc=='Cliente':
-            command = f"INSERT INTO {table} (nombre, ap_p, ap_m, phone, email) VALUES ('{obj.nombre}', '{obj.ap_p}', '{obj.ap_m}', '{obj.phone}', '{obj.email}';)"
+            command = f"INSERT INTO {table} (nombre, ap_p, ap_m, phone, email) VALUES ('{obj.nombre}', '{obj.ap_p}', '{obj.ap_m}', '{obj.phone}', '{obj.email}');"
         elif opc=='Proveedor':
-            command = f"INSERT INTO {table} (nombre, phone, email) VALUES ('{obj.nombre}', '{obj.phone}', '{obj.email}';)"
+            command = f"INSERT INTO {table} (nombre, phone, email) VALUES ('{obj.nombre}', '{obj.phone}', '{obj.email}');"
         elif opc=='Sucursal':
-            command = f"INSERT INTO {table} (nombre, direccion, phone) VALUES ('{obj.nombre}', '{obj.direccion}', '{obj.phone}';)"
+            command = f"INSERT INTO {table} (nombre, direccion, phone) VALUES ('{obj.nombre}', '{obj.direccion}', '{obj.phone}');"
 
         return command
